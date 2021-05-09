78e6eaa952f406a1a47830cb14f8772acc207b41
KotoGit <gsreceiver@gmail.com>
2021-05-08 23:19:49 -0400

    Updates

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-declare var possibleEvents: Set<string>;
-declare class TickInputHandler {
-    static epochTicks: number;
-    static ticksPerMillisecond: number;
-    static maxDateMilliseconds: number;
-    showResult(inputElement: HTMLInputElement): string;
-}
